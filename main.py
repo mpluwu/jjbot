2d8c27b6c82dcc85c345966c0d645b4a12737b1b
pluto <2006dadashova@gmail.com>
2026-01-08 18:44:30 +0400

    changed my dc name

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ async def slash_command(interaction:discord.Interaction):
 
 @bot.tree.command(name="intro",description="Introduction of himself")
 async def slash_command(interaction:discord.Interaction):
-    await interaction.response.send_message("Hi, I'm Jeyhun and I was created by @tspmodamn on Discord. I used to work in a bank company, but because of the civil war, I escaped the real world and now I'm connected thanks to the Internet. If you see Damian, please, just say my name.")
+    await interaction.response.send_message("Hi, I'm Jeyhun and I was created by @cuzimmrvain on Discord. I used to work in a bank company, but because of the civil war, I escaped the real world and now I'm connected thanks to the Internet. If you see Damian, please, just say my name.")
     
 @bot.tree.command(name="hello",description="Hello-es you")
 async def slash_command(interaction:discord.Interaction,mention:discord.Member):
@@ -151,5 +151,6 @@ async def rps(interaction: discord.Interaction, choose: str):
         return
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
+
 
 
